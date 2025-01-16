24b785bf780895ad7760a8566f423187b3099622
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-01-16 16:34:45 +0200

    completed js file and tests with Math.round() - you can read about that again at https://stackoverflow.com/questions/7342957/how-do-you-round-to-one-decimal-place-in-javascript

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let output = ((temperature - 32) * (5/9));  
+  let rounded = Math.round(output *10) / 10; 
+  return rounded; 
 };
 
-const convertToFahrenheit = function() {
-};
-
+const convertToFahrenheit = function(temperature) {
+  let output = ((temperature * (9/5) + 32));  
+  let rounded = Math.round(output * 10) / 10; 
+  return rounded; 
+}; 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
