37419b65ef5bcb589f769544991312f17ce44581
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-05-29 20:13:20 +0200

    completed palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const arr = str
+    .toLowerCase()
+    .split('') 
+    .filter((char) => alphanumerical.includes(char))
+    .join(''); 
 
+  const  reversed = arr.split('').reverse().join('');
+  return  arr === reversed;
 };
 
 // Do not edit below this line
