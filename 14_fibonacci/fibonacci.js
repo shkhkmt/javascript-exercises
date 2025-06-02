4bea81a8c6910b3d9d2af4d97f2f2b9ce8b33c49
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-06-02 15:29:01 +0200

    completed fibonacci test

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+  let count; 
+  // make sure the input argument is a value.
+  if (typeof countArg !== 'number') {
+    count = parseInt(countArg)
+    }
+  else {
+    count = countArg; 
+  }
 
+  if (count < 0) return "OOPS"; 
+  if (count === 0) return 0; 
+
+  // define arr with 1st two points in sequence;
+  let arr = [ 0, 1 ];
+  // loop from the 1st unknown position
+  for (let i = 2; i <= count; i++) {
+    // add the last two positions 
+    arr[i] = arr[i-1] + arr[i-2]; 
+  }
+  return arr[count];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
